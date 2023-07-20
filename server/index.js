e50972eb46a66a37cafcb85b8024ecf5dfb1ed5b
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-20 22:52:22 +0100

    init mysql connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "EmpSys"
+});
+
 
 app.listen(3001, () => {
     console.log('Server is Running on port 3001');
